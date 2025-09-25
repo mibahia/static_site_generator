d30f3731e643dd98024522e0cc59f1f88b4c1481
mibahia <mariaizabel.bahia@gmail.com>
2025-09-25 09:15:20 +0100

    fix main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from copy_content import copy_content
+from generate_page import generate_pages_recursive
+
+
 def main():
-    print("Hello from static-site-generator!")
+    copy_content("static", "public")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 
 if __name__ == "__main__":
